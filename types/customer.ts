f2341c78e2706289228cd123c0c116ad5195af88
splicetecbayern24-dotcom <splicetecbayern24@gmail.com>
2026-03-17 13:04:33 +0100

    Create customer.ts

---
+++ types/customer.ts
@@ -0,0 +1,9 @@
+export type Customer = {
+  id: string;
+  company_name: string;
+  contact_name?: string | null;
+  email?: string | null;
+  phone?: string | null;
+  city?: string | null;
+  address?: string | null;
+};
