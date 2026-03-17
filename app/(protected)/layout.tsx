1f7c815dc4a31271fee37b9c7be17d047ce40090
splicetecbayern24-dotcom <splicetecbayern24@gmail.com>
2026-03-17 14:33:46 +0100

    Create layout.tsx

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from "react";
+import { AppShell } from "@/components/layout/AppShell";
+
+export default function ProtectedLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <AppShell>{children}</AppShell>;
+}
