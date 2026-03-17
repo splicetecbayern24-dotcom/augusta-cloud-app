10da472d9544fe9b2b6eed33c5e93d553b8a8bf3
splicetecbayern24-dotcom <splicetecbayern24@gmail.com>
2026-03-17 14:05:08 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { Dashboard } from "../components/dashboard";
+import { redirect } from "next/navigation";
 
-export default function Page() {
-  return <Dashboard />;
+export default function HomePage() {
+  redirect("/rechnungen");
 }
